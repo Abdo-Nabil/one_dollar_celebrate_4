3aa46ed5ce6fcd1044a99edd76adcbf36a7eff66
Abdo Nabil <abdo.sw.eng@gmail.com>
2022-10-08 23:52:57 +0200

    upload

---
+++ android/app/src/main/kotlin/com/AbdoNabil/one_dollar_celebrate_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.AbdoNabil.one_dollar_celebrate_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_home_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'constants.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+  bool isActive = false;
+
+  @override
+  void initState() {
+    //
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 3733));
+    //
+    _controller.addStatusListener(
+      (status) {
+        if (status == AnimationStatus.completed) {
+          debugPrint('Animation completed');
+          setState(() {
+            isActive = false;
+          });
+          _controller.reset();
+        }
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(AppStrings.oneDollarApp),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  AppStrings.congratulations,
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                // SizedBox(
+                //   height: MediaQuery.of(context).size.height * 0.05,
+                // ),
+                Stack(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.50,
+                      width: double.infinity,
+                      child: Lottie.asset(AppStrings.celebrationBoxPath),
+                    ),
+                    GestureDetector(
+                      child: isActive
+                          ? SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.50,
+                              width: double.infinity,
+                              child: Center(
+                                child: Lottie.asset(
+                                  AppStrings.celebrationPath,
+                                  animate: false,
+                                  controller: _controller,
+                                  // onLoaded: (composition) {
+                                  //   _controller.duration = composition.duration;
+                                  //   debugPrint(composition.duration.inMilliseconds
+                                  //       .toString());
+                                  // },
+                                ),
+                              ),
+                            )
+                          : Container(
+                              width: double.infinity,
+                              height: MediaQuery.of(context).size.height * 0.50,
+                              color: Colors.white.withOpacity(0.55),
+                            ),
+                      onTap: () {
+                        setState(() {
+                          isActive = true;
+                          debugPrint('pressed');
+                        });
+                        _controller.forward();
+                      },
+                    ),
+                  ],
+                ),
+                const Text(
+                  AppStrings.clickOnTheBox,
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
